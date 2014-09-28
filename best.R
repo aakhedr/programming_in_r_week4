b8502b91129dd32baaf8da6aa9d638dd8f3a343f
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-09-28 22:32:24 +0200

    add rankall.R without NAs

--- best.R
+++ best.R
@@ -10,7 +10,8 @@ match_outcome <- function(string, strings) {
 
 best <- function(state, outcome) {
         ## read outcome date
-        data <- read.csv("outcome-of-care-measures.csv", na.string="Not Available")
+        data <- read.csv("outcome-of-care-measures.csv", 
+                         na.string="Not Available")
         names <- names(data)
 
         ## check that state and outcome are valid
@@ -22,7 +23,8 @@ best <- function(state, outcome) {
         if (!is.element(state, states)) {
                 stop("invalid state")
         }
-        ## return the hospital name in the state with the lowest 30 day-death rate
+        ## return the hospital name in the state with the lowest 30 day-death 
+        ## rate
         data_subset <- subset(data, data$State==state)
         ordered <- data_subset[order(data_subset[[matched]]), ]
         return(as.character(ordered[1:1, "Hospital.Name"]))
---
+++ rankall.R
@@ -0,0 +1,57 @@
+match_outcome <- function(string, strings) {
+        string <- tolower(gsub(" ", ".", string))
+        
+        for (i in 1:length(strings)) {
+                if (grepl(string, tolower(strings[i]))) {
+                        return(strings[i])
+                }
+        }
+}
+
+rankall <- function(outcome, num="best") {
+        ## read outcome date
+        data <- read.csv("outcome-of-care-measures.csv", 
+                         na.string="Not Available")
+        names <- names(data)
+        
+        ## check that state and outcome are valid
+        matched <- match_outcome(outcome, names)
+        if (is.null(matched)) {
+                stop("invalid outcome")
+        }
+        ## return the hospital name in the state with the lowest 30 day-death 
+        ## rate
+        ordered <- data[order(data[["State"]], data[[matched]], 
+                              data[["Hospital.Name"]]), ]
+        states <- as.character(unique(ordered[, "State"]))
+        ordered[["Rank"]] <- 0
+        k <- 1          ## index into the 4706 observations
+        for (i in 1:length(states)) {
+                ordered_subset <- subset(ordered, ordered$State==states[i])
+                for (j in 1:nrow(ordered_subset)) {
+                        ordered[k, "Rank"] <- j
+                        k <- k + 1
+                }
+        }
+        if (num == "best") {
+                num <- 1
+        }
+        else if (num == "worst") {
+                hospitals <- character()
+                for (i in 1:length(states)) {
+                        ordered_subset <- subset(ordered, ordered$State==states[i])
+                        num <- nrow(ordered_subset)
+                        while (is.na(ordered_subset[num, matched])) {
+                                num <- num - 1
+                        }
+                        hospital <- as.character(ordered[ordered$Rank==num & 
+                                        ordered$State==states[i], "Hospital.Name"])
+                        hospitals <- c(hospitals, hospital)
+                }
+                return(data.frame(hospital=hospitals, state=states, 
+                                  row.names=states))
+        }
+        hospitals <- as.character(ordered[ordered$Rank==num, "Hospital.Name"])
+        st <- ordered[ordered$Rank==num, "State"]
+        return(data.frame(hospital=hospitals, state=st, row.names=st))
+}
