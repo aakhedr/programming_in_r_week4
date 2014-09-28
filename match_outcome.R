f6c765387e449d9a3e658c99a4d9ea2ab0e4fa04
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-09-28 04:09:54 +0200

    removed match_outcome file, function in best.R

--- match_outcome.R
+++
@@ -1,9 +0,0 @@
-match_outcome <- function(string, strings) {
-        string <- tolower(gsub(" ", ".", string))
-        
-        for (i in 1:length(strings)) {
-                if (grepl(string, tolower(strings[i]))) {
-                        return(strings[i])
-                }
-        }
-}
